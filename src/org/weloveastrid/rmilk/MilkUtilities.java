2f7089ea5e4bae7729cd4f701bfebbdcd102aa40
Tim Su <tim@todoroo.com>
2010-08-25 23:10:41 -0700

    Now we can read old token from astrid on new install

--- src/org/weloveastrid/rmilk/MilkUtilities.java
+++ src/org/weloveastrid/rmilk/MilkUtilities.java
@@ -3,6 +3,11 @@
  */
 package org.weloveastrid.rmilk;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import com.todoroo.andlib.service.ContextManager;
 import com.todoroo.astrid.sync.SyncProviderUtilities;
 
 /**
@@ -23,7 +28,28 @@ public class MilkUtilities extends SyncProviderUtilities {
 
     // --- utilities boilerplate
 
-    @Override
+    private MilkUtilities() {
+        // if no token is set, see if astrid has exported one
+        if(getToken() == null) {
+            try {
+                Context astridContext = ContextManager.getContext().createPackageContext("com.timsu.astrid", 0);
+                SharedPreferences sharedPreferences = astridContext.getSharedPreferences("rtm", 0);
+                if(sharedPreferences != null) {
+                    String token = sharedPreferences.getString("rmilk_token", null);
+                    long lastSyncDate = sharedPreferences.getLong("rmilk_last_sync", 0);
+
+                    Editor editor = getPrefs().edit();
+                    editor.putString(getIdentifier() + PREF_TOKEN, token);
+                    editor.putLong(getIdentifier() + PREF_LAST_SYNC, lastSyncDate);
+                    editor.commit();
+                }
+            } catch (Exception e) {
+                // too bad
+            }
+        }
+    }
+
+;    @Override
     public String getIdentifier() {
         return IDENTIFIER;
     }
